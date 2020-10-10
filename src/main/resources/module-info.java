e0bd3ff25082c5ec3780b4e4166fbe9e74657647
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-10 12:04:56 +0200

    JPAopdracht1

---
+++ src/main/resources/module-info.java
@@ -0,0 +1,8 @@
+open module eu.noelvaes.jpa{
+
+    requires java.persistence;
+    requires org.hibernate.orm.core;
+    requires java.sql;
+    requires net.bytebuddy;
+    requires java.xml.bind;
+}
